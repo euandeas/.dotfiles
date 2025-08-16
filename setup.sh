e1aaeb7c0e58d516ad095d84ca6e20ed2c84a0be
Euan Deas <contact@euandeas.com>
2025-08-16 17:45:32 +0100

    Add DNS and packages to setup script

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@
 check_sudo() {
     if ! sudo -n true 2>/dev/null; then
         echo "This script requires sudo privileges."
-        echo "Please enter your password:"
         if ! sudo -v; then
             echo "Failed to obtain sudo privileges"
             exit 1
@@ -13,13 +12,27 @@ check_sudo() {
 }
 
 check_sudo
-echo "Sudo privileges confirmed. Running script..."
+echo " "
+echo "Starting setup..."
+echo " "
 
-omarchy-theme-install https://github.com/euandeas/omarchy-flexoki-light-theme.git
+echo "Configuring DNS:"
+cat ~/.dotfiles/resolved.conf
+sudo cp ~/.dotfiles/resolved.conf /etc/systemd/
+sudo systemctl restart systemd-resolved
+echo " "
+
+omarchy-theme-install https://github.com/euandeas/omarchy-flexoki-light-theme.git > /dev/null 2>&1
 echo "Flexoki Light Theme installed."
 echo " "
-omarchy-theme-install https://github.com/euandeas/omarchy-flexoki-dark-theme.git
+
+omarchy-theme-install https://github.com/euandeas/omarchy-flexoki-dark-theme.git > /dev/null 2>&1
 echo "Flexoki Dark Theme installed."
 echo " "
 
-yay -S brave ghostty zed obsidian-bin discord libreoffice-fresh stow
+echo "Sym-Linking Config Files"
+stow -v */
+echo " "
+
+echo "Installing Packages"
+yay -S --noconfirm --needed brave ghostty zed obsidian-bin discord libreoffice-fresh stow syncthing-bin starship

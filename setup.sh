ab33e02bc863faca094d0b6f5b2673d33f91d9b5
Euan Deas <contact@euandeas.com>
2025-08-20 20:47:33 +0100

    fix stow to overwrite files

--- setup.sh
+++ setup.sh
@@ -2,13 +2,13 @@
 
 # Function to check if user has sudo privileges
 check_sudo() {
-    if ! sudo -n true 2>/dev/null; then
-        echo "This script requires sudo privileges."
-        if ! sudo -v; then
-            echo "Failed to obtain sudo privileges"
-            exit 1
-        fi
+  if ! sudo -n true 2>/dev/null; then
+    echo "This script requires sudo privileges."
+    if ! sudo -v; then
+      echo "Failed to obtain sudo privileges"
+      exit 1
     fi
+  fi
 }
 
 check_sudo
@@ -22,18 +22,19 @@ sudo cp ~/.dotfiles/resolved.conf /etc/systemd/
 sudo systemctl restart systemd-resolved
 echo " "
 
-omarchy-theme-install https://github.com/euandeas/omarchy-flexoki-light-theme.git > /dev/null 2>&1
+omarchy-theme-install https://github.com/euandeas/omarchy-flexoki-light-theme.git >/dev/null 2>&1
 echo "Flexoki Light Theme installed."
 echo " "
 
-omarchy-theme-install https://github.com/euandeas/omarchy-flexoki-dark-theme.git > /dev/null 2>&1
+omarchy-theme-install https://github.com/euandeas/omarchy-flexoki-dark-theme.git >/dev/null 2>&1
 echo "Flexoki Dark Theme installed."
 echo " "
 
 yay -S --noconfirm --needed stow
 
 echo "Sym-Linking Config Files"
-stow -v */
+stow --adopt -v */
+git restore .
 echo " "
 
 echo "Installing Packages"

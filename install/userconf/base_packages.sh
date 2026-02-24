3793dba513894f3b2b8a9111706bbb3c1db5b1d3
euandeas <contact@euandeas.com>
2026-02-24 09:14:19 -0500

    add tailscale

--- install/userconf/base_packages.sh
+++ install/userconf/base_packages.sh
@@ -5,16 +5,20 @@
 # -----------------------------
 sudo dnf config-manager addrepo --overwrite --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo
 sudo dnf config-manager addrepo --overwrite --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+sudo dnf config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
 wget "https://repo.protonvpn.com/fedora-$(cat /etc/fedora-release | cut -d' ' -f 3)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.3-1.noarch.rpm"
 sudo dnf install ./protonvpn-stable-release-1.0.3-1.noarch.rpm -yq
 rm ./protonvpn-stable-release-1.0.3-1.noarch.rpm
 sudo dnf makecache --refresh
+
 
 # -----------------------------
 # DNF
 # -----------------------------
 sudo dnf install -yq $(grep -vE '^\s*#|^\s*$' "$DOTS_INSTALL/dnf.packages")
 sudo dnf install gh --repo gh-cli -yq
+
+sudo systemctl enable --now tailscaled
 
 # -----------------------------
 # Flatpak
